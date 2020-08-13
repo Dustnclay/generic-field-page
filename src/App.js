e384a33303308c4e39754d005cd9ed444c0fcc2b
Dustin Tiner <60906954+Dustnclay@users.noreply.github.com>
2020-08-12 22:02:29 -0500

    Update App.js
    learned astronomer added

--- src/App.js
+++ src/App.js
@@ -14,41 +14,19 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
+      <h1>Come one, Come all, gather round and hear the tale of the Learnd Astronomer</h1>
+      <h3>
+        When I heard the learn’d astronomer,<br/>
+        When the proofs, the figures, were ranged in columns before me,<br/>
+        When I was shown the charts and diagrams, to add, divide, and measure them,<br/>
+        When I sitting heard the astronomer where he lectured with much applause in the lecture-room,<br/>
+        How soon unaccountable I became tired and sick,<br/>
+        Till rising and gliding out I wander’d off by myself,<br/>
+        In the mystical moist night-air, and from time to time,<br/>
+        Look’d up in perfect silence at the stars.<br/>
+      <h3>
       <br />
-      <h2>The date according to Go is:</h2>
+      <h2>The date is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
